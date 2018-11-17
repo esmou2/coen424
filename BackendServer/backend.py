d72079524c745176eb6114694a56c695b23c688d
Alireza <alirezak2009@hotmail.com>
2018-11-17 16:48:22 -0500

    Add files via upload

---
+++ BackendServer/backend.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def about():
+    conn = sqlite3.connect('database\\books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM books;').fetchall()
+
+    return jsonify(all_books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('C:\\Users\\Alire\\Documents\\School\\COEN424\\Project\\myproject\\database\\books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
