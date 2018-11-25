609a9a2ce0cc4d29c88afd589ebda6129fd13e10
Esma <esma.mouine.1@ulaval.ca>
2018-11-24 23:44:53 -0500

    almost

--- spark-app/engine.py
+++ spark-app/engine.py
@@ -1,9 +1,25 @@
+from pyspark.sql.functions import udf
+from pyspark.sql.types import DoubleType
+
+
 class RecommendationEngine:
 
     def count(self):
         return self.df.count()
 
+    def label_for_results(s):
+        if s == 'failed':
+            return 0.0
+        elif s == 'successful':
+            return 1.0
+        else:
+            return -1.0
+
     def __init__(self, ss):
         self.ss = ss
         self.df = ss.read.format("com.mongodb.spark.sql.DefaultSource").load()
         self.df.printSchema()
+        label = udf(self.label_for_results, DoubleType())
+        labeled_data = self.df.select(label("state").alias('label'), "duration", "usd_goal_real") \
+            .where('label >= 0')
+        labeled_data.take(1).show()
--- spark-app/test.py
+++ spark-app/test.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from pyspark.ml.recommendation import ALS
 from pyspark.sql import SparkSession
 
 app = Flask(__name__)
@@ -7,6 +8,8 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def hello():
     return "YOYO"
+
+model = ALS(userCol="userId", itemCol="movieId", ratingCol="rating").fit(ratings)
 
 
 if __name__ == '__main__':
