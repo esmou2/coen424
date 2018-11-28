39bdbb59d67ea5b815fe3d02db8684b5f911c7eb
Esma <esma.mouine.1@ulaval.ca>
2018-11-28 16:30:50 -0500

    add cors

--- spark-app/app.py
+++ spark-app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, json
+from flask_restful.utils.cors import crossdomain
 
 from engine import RecommendationEngine
 
@@ -6,11 +7,13 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
+@crossdomain(origin='*')
 def test():
     return "hey"
 
 
 @app.route("/", methods=["POST"])
+@crossdomain(origin='*')
 def send_recommendation():
     return json.dumps(recommendation_engine.get_prediction(request.get_json()))
 
