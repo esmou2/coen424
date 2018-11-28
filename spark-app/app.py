262980c47c91bf770dba149928fe702ea6481b51
Esma <esma.mouine.1@ulaval.ca>
2018-11-28 18:11:32 -0500

    handle errors

--- spark-app/app.py
+++ spark-app/app.py
@@ -17,9 +17,9 @@ def send_recommendation():
     print("TEST   ", request)
     print("TEST   ", request.get_json())
     print("TEST   ", request.data)
-    # if request.data.get_json():
-    return json.dumps(recommendation_engine.get_prediction(request.get_json()))
-    # return json.dumps({"error": "bad data format"}), 500
+    if request.data.get_json():
+        return json.dumps(recommendation_engine.get_prediction(request.get_json()))
+    return json.dumps({"error": "bad data format"}), 500
 
 
 def create_app(spark_session):
