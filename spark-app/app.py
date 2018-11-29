6e65c475204f6b27aa982dd75f46abb40049a95f
Esma <esma.mouine.1@ulaval.ca>
2018-11-28 21:17:22 -0500

    vount cat by state

--- spark-app/app.py
+++ spark-app/app.py
@@ -16,10 +16,8 @@ def test():
 def send_recommendation():
     received_data = request.get_json()
     if received_data and set(received_data.keys()) == {"category", "main_category", "duration", "usd_goal_real"}:
-        prediction, state_count, m_cat_count, m_cat_count_state = recommendation_engine.get_prediction(received_data)
-        res = {"prediction": prediction, "state_count": state_count, "m_cat_count": m_cat_count,
-               "m_cat_count_state": m_cat_count_state
-               }
+        res = recommendation_engine.get_prediction(received_data)
+
         return json.dumps(res)
     return json.dumps({"error": "bad data format"}), 400
 
