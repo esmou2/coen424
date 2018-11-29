7293edc476cfa5bf57dbee02a3f5ce4226ec271c
Esma <esma.mouine.1@ulaval.ca>
2018-11-28 23:19:26 -0500

    fix

--- spark-app/engine.py
+++ spark-app/engine.py
@@ -15,8 +15,7 @@ class RecommendationEngine:
     def _extract_metrics(self, m_cat_count, m_cat_count_state, m_cat_sum_goals):
         data = {"per_failed_project": self.per_failed_project, "per_successful_project": self.per_successful_project,
                 "per_projects_in_cat": (m_cat_count[0][1] * 100) / self.count,
-                "avg_goal_usd": m_cat_sum_goals[0][1] / m_cat_count[0][1],
-                "acc": self.accuracy}
+                "avg_goal_usd": m_cat_sum_goals[0][1] / m_cat_count[0][1]}
 
         if "failed" in m_cat_count_state[0]:
             data["per_failed_project"] = (m_cat_count_state[0][2] * 100) / m_cat_count[0][1]
